bff02618ff1c24db51e9140561136427e26c6823
cjogles <jackson.ogles28@gmail.com>
2019-10-02 00:56:57 -0600

    finished mvp and the event listeners really make the page go crazy!

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 /*   
-	* [ ] `mouseover`
+	* [x] `mouseover`
 	* [ ] `keydown`
 	* [ ] `wheel`
 	* [ ] `drag / drop`
@@ -19,12 +19,12 @@
 // user hovers mouse. | After event happens the image is targeted via ID
 // and the style.transitionProperty property is used with param's of
 // width and height which have been declared inside mix-in.less file.
+// I put the script tag at the bottom of the html page to make sure the 
+// function was at least undefined instead of null. 
 
-const funBus = document.getElementById("fun-bus").addEventListener(
-    "mouseover", function() {
-        document.getElementById("fun-bus").style.transitionProperty = "width,height";
-    }
-)
+const sizeChange = document.getElementById("fun-bus").addEventListener("mouseover", () => {
+    document.getElementById("fun-bus").style.transitionProperty = "width,height";
+});
 
 // number 2
 // ****** When user attempts to copy and paste paragraphs  *********
@@ -46,9 +46,56 @@ copyright.forEach((element) => {
 })
 
 // number 3
-// **** make the map picture wiggle when you click on it ***
-//
+// **** make the map picture rotate 180deg when you double click on it ***
+// target adventure class and add listener with dblclick event and then target 
+// adventure class again and change its style.transform to rotate 180 deg
 
-document.getElementById("adventure").addEventListener("click", () => {
-    
+document.getElementById("adventure").addEventListener("mousedown", () => {
+    document.getElementById("adventure").style.transform = "rotate(180deg)";
+})
+
+// number 4
+// Comes back up
+document.getElementById("adventure").addEventListener("mouseup", () => {
+    document.getElementById("adventure").style.transform = "rotate(180deg)";
+})
+
+// number 5
+// ************** make nav background dark mode on scroll ************
+
+document.getElementById("text-content").addEventListener("mouseenter", () => {
+    document.getElementById("text-content").style.backgroundColor = "red";
+    document.getElementById("bodysection").style.backgroundColor ="darkgrey";
+})
+
+// number 6
+// ****************  make background pink upon leaving ***********
+
+document.getElementById("text-content").addEventListener("mouseleave", () => {
+    document.getElementById("text-content").style.backgroundColor = "black";
+    document.getElementById("bodysection").style.backgroundColor ="pink";
+})
+
+// number 7
+// *************** change nav bar header area to rebecca purple *********
+
+document.getElementById("headerstuff").addEventListener("mouseleave", () => {
+    document.getElementById("headerstuff").style.backgroundColor = "rebeccapurple";
+})
+
+// number 8
+
+document.getElementById("one").addEventListener("mouseover", () => {
+    document.getElementById("one").style.transform = "scale(3.3)";
+})
+// number 9
+
+document.getElementById("two").addEventListener("mouseover", () => {
+    event.stopPropagation();
+    document.getElementById("two").style.transform = "scale(2.2)";
+})
+// number 10
+
+document.getElementById("three").addEventListener("mouseover", () => {
+    document.getElementById("three").style.transform = "scale(1.1)";
 })
